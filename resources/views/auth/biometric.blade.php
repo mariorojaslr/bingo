d74ac3101b603b12d0721fa796d6b5a81249a8c9
mariorojaslr <83791643+mariorojaslr@users.noreply.github.com>
2026-03-24 19:46:17 -0300

    fix(auth): Inyeccion de Token CSRF para validar POST biometrico

--- resources/views/auth/biometric.blade.php
+++ resources/views/auth/biometric.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verificación Biométrica Requerida</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
