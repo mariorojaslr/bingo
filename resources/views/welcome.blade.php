5dd8b6f6c6e957ba313c77fb938899ad056e8e0c
mariorojaslr <83791643+mariorojaslr@users.noreply.github.com>
2026-08-13 16:40:35 -0300

    Fix: horizontal scroll issue (100vw bug on Windows browsers)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
             position: fixed;
             top: 0;
             left: 0;
-            width: 100vw;
+            width: 100%;
             height: 100vh;
             z-index: -1;
             background: radial-gradient(circle at 50% 10%, rgba(0, 255, 136, 0.08) 0%, transparent 40%),
